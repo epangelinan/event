6a9be15c4f926b2c14a173974792540bfd2ad466
Esvethlynna Pangelinan <esveth@aol.com>
2017-06-30 12:15:21 -0700

    add code to display the user's selections

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -9,11 +9,14 @@ public class App {
     System.out.println("Welcome to the Event Planner.  Please answer the following questions to start planning your event.");
 
     System.out.println("How many guests will be attending?");
-    Integer numOfGuests = Integer.parseInt(myConsole.readLine());
+    int numOfGuests = Integer.parseInt(myConsole.readLine());
 
 
     boolean programRunning = true;
     List<String> userFoodMenu = new ArrayList<String>();
+    List<String> userBeverageMenu = new ArrayList<String>();
+    List<String> userEntertainmentMenu = new ArrayList<String>();
+
     String userChoice;
 
     while(programRunning) {
@@ -26,6 +29,68 @@ public class App {
       }
     }
 
-//Display the Event details and estimated cost for the user to review
+    System.out.println(userFoodMenu);
+
+    programRunning = true;
+    while(programRunning) {
+      System.out.println("Please enter the beverages that you want for the party.  Enter Exit after you are done.");
+      userChoice = myConsole.readLine();
+      if (userChoice.equals("Exit")) {
+        programRunning = false;
+      } else {
+        userBeverageMenu.add(userChoice);
+      }
+    }
+
+    System.out.println(userBeverageMenu);
+
+    System.out.println("Would you like to add entertainment to your party? Please enter Yes or No");
+    String entertainmentAnswer = myConsole.readLine();
+
+    if (entertainmentAnswer.equals("Yes")) {
+      programRunning = true;
+      while(programRunning) {
+        System.out.println("Please enter the entertainment that you want for the party.  Enter Exit after you are done.");
+        userChoice = myConsole.readLine();
+        if (userChoice.equals("Exit")) {
+          programRunning = false;
+        } else {
+          userEntertainmentMenu.add(userChoice);
+        }
+      }
+    } else {
+      userEntertainmentMenu.add("None");
+    }
+
+    System.out.println(userEntertainmentMenu);
+
+    Event userEvent = new Event(numOfGuests, userFoodMenu, userBeverageMenu, userEntertainmentMenu);
+
+    System.out.println("Thanks!  Here are your event details:");
+    System.out.println("Number of Guests");
+    System.out.println("----------------");
+    System.out.println(userEvent.getNumberOfGuests());
+    System.out.println("");
+
+    System.out.println("Food Menu");
+    System.out.println("---------");
+    for (String individualFood: userFoodMenu) {
+      System.out.println(individualFood);
+    }
+    System.out.println("");
+
+    System.out.println("Beverages Menu");
+    System.out.println("--------------");
+    for (String individualBeverage: userBeverageMenu) {
+      System.out.println(individualBeverage);
+    }
+    System.out.println("");
+
+    System.out.println("Entertainment");
+    System.out.println("-------------");
+    for (String individualEntertainment: userEntertainmentMenu) {
+      System.out.println(individualEntertainment);
+    }
+    System.out.println("");
   }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,10 +1,17 @@
+import java.util.List;
+import java.util.ArrayList;
+
 class Event {
   private int mNumberOfGuests;
-  private String[] mFoodMenu = {"Salmon Burgers", "Tortilla Pizza", "Cod with a Crab and Herb Crust", "Chermoula Chicken", "Mango Lime Tarts", "Cherry Crumble", "Classic New York Cheesecake"; "Raspberry Crisp", "Lavender Cake"};
+  private String[] mFoodMenu = {"Salmon Burgers", "Tortilla Pizza", "Cod with a Crab and Herb Crust", "Chermoula Chicken", "Mango Lime Tarts", "Cherry Crumble", "Classic New York Cheesecake", "Raspberry Crisp", "Lavender Cake"};
   private String[] mBeveragesMenu = {"Coffee", "Tea", "Wine", "Soda","Full Bar"};
   private String[] mEntertainmentMenu = {"Live Band", "Games", "Dancing"};
+  //private List<String> mFood;
+  //private List<String> mBeverages;
+  //private List<String> mEntertainment;
+
   private List<String> mFood = new ArrayList<String>();
-  private List<String> mBeverages= new ArrayList<String>();
+  private List<String> mBeverages = new ArrayList<String>();
   private List<String> mEntertainment = new ArrayList<String>();
 
   public int getNumberOfGuests() {
@@ -23,21 +30,30 @@ class Event {
     return mEntertainmentMenu;
   }
 
-  public 
+
 
   public Event(int numberOfGuests, List<String> food, List<String> beverages, List<String> entertainment) {
     mNumberOfGuests = numberOfGuests;
+    String indexFood;
+    String indexBeverages;
+    String indexEntertainment;
+    mFood.addAll(food);
+    mBeverages.addAll(beverages);
+    mEntertainment.addAll(entertainment);
 
-    for (Integer index = 0 ; index < food.length ; index++) {
-      mFood[index] = food[index];
-    }
-
-    for (Integer index = 0 ; index < beverages.length ; index++) {
-      mBeverages[index] = beverages[index];
-    }
-
-    for (Integer index = 0 ; index < entertainment.length ; index++) {
-      mEntertainment[index] = entertainment[index];
-    }
+    // for (Integer index = 0 ; index < food.length; index++) {
+    //   indexFood = food[index];
+    //   mFood[index] = indexFood;
+    // }
+    //
+    // for (Integer index = 0 ; index < beverages.length; index++) {
+    //   indexBeverages = beverages[index];
+    //   mBeverages[index] = indexBeverages;
+    // }
+    //
+    // for (Integer index = 0 ; index < entertainment.length; index++) {
+    //   indexEntertainment= entertainment[index];
+    //   mEntertainment[index] = indexEntertainment;
+    // }
   }
 }
